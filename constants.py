9c261c123748c423aafd43eb77ff9366200e3435
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-03-14 12:17:50 +0200

    Add event parking constants

--- constants.py
+++ constants.py
@@ -2,13 +2,12 @@ from os import environ
 
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(".env", override=True)
 
 TIMEFORMAT = "%Y-%m-%dT%H:%MZ"
 PARKKI_HOST = environ.get("PARKKI_HOST", None)
 PARKKI_HTTP_HOST = environ.get("PARKKI_HTTP_HOST", None)
 PARKKIOPAS_HOST = environ.get("PARKKIOPAS_HOST", None)
-
 HEADERS = {
     "Content-Type": "application/json",
     "Authorization": f"ApiKey {environ.get('TOKEN_KEY', None)}",
@@ -19,3 +18,5 @@ TEST_EXTERNAL_ID = environ.get("TEST_EXTERNAL_ID", None)
 TEST_PERMIT_SERIES_ID = environ.get("TEST_PERMIT_SERIES_ID", None)
 TEST_PERMIT_AREA_IDENTIFIER_1 = environ.get("TEST_PERMIT_AREA_IDENTIFIER_1", None)
 TEST_PERMIT_AREA_IDENTIFIER_2 = environ.get("TEST_PERMIT_AREA_IDENTIFIER_2", None)
+TEST_EVENT_PARKING = environ.get("TEST_EVENT_PARKING", False)
+TEST_EVENT_AREA_ID = environ.get("TEST_EVENT_AREA_ID", None)
