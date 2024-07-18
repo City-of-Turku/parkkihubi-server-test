452a3c46cee0d0fd8ada912f2923f6ae356699c9
juuso-j <68938778+juuso-j@users.noreply.github.com>
2024-07-18 10:27:03 +0300

    Add str_to_bool

--- utils.py
+++ utils.py
@@ -10,3 +10,12 @@ def key_in_list_of_dicts(key_to_find, list_of_dicts):
         if key_to_find in d:
             return True
     return False
+
+
+def str_to_bool(s):
+    if s.lower() in ["true", "1", "t", "y", "yes"]:
+        return True
+    elif s.lower() in ["false", "0", "f", "n", "no"]:
+        return False
+    else:
+        raise ValueError(f"Invalid truth value: {s}")
